985be8c251e400d9036540592401610f24e66a53
vardhan0604 <harsh062004@gmail.com>
2025-12-14 16:47:24 +0530

    feat(frontend): add ProtectedRoute to secure dashboard access

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* Protected Dashboard */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </Router>
   );
